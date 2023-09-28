bd0f34e08a1e50f8b4e6660de017cd36231e826b
Jpazda <111332675+Jpazda@users.noreply.github.com>
2023-09-28 01:00:27 +0000

    First Commit

--- src/models.py
+++ src/models.py
@@ -7,26 +7,48 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(12), nullable=False)
+    first_name = Column(String(20), nullable=False)
+    last_name = Column(String(20), nullable=False)
+    posts = relationship("Post", backref="authored_posts")
+    mentions = relationship("Post", backref="mentioned_user")
+    
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    author_id = Column(Integer, ForeignKey("user.id"))
+    comments = relationship("Comment", backref="post")
+    mentioned_id = Column(Integer, ForeignKey("user.id"))
+    
+
 
     def to_dict(self):
         return {}
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    author_id = Column(Integer, ForeignKey("user.id"))
+    post_id = Column(Integer, ForeignKey("post.id"))
+
+class Follower(Base):
+    __tablename__='follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey("user.id"))
+
+class Media(Base):
+    __tablename__='media'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey("post.id"))
+    url = Column(String(250), nullable=False)
 
 ## Draw from SQLAlchemy base
 try:
